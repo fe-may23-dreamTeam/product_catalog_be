234f912e86bf5c8f2fec6e6d025ed56cceb146ee
MaxSchmide <kuznetsov.max.v@gmail.com>
2023-08-31 13:07:01 +0300

    fixed tsconfig

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -5,6 +5,6 @@ export const mongooseConnect = () => {
   if (mongoose.connection.readyState === 1) {
     return mongoose.connection.asPromise();
   } else {
-    return mongoose.connect(process.env.MONGODB_URI!);
+    return mongoose.connect(process.env.MONGODB_URI);
   }
 };
