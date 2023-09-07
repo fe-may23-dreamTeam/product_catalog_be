aa9e7e182622fd61e0246a93f16484d73a8ebb4b
Max Kuznetsov <93060640+MaxSchmide@users.noreply.github.com>
2023-09-07 22:43:57 +0300

    Update corsOptions.ts

--- src/utils/corsOptions.ts
+++ src/utils/corsOptions.ts
@@ -3,7 +3,7 @@ import { CorsCallback } from '../types/CorsCallback';
 const whitelist = ['http://localhost:3000', 'https://funny-shop.netlify.app'];
 
 export const corsOptions = {
-  origin: *
+  origin: ''
   //   (origin: string, callback: CorsCallback) => {
   //   if (whitelist.indexOf(origin) !== -1) {
   //     callback(null, true);
